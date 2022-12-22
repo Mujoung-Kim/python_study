6fc34fd589870bb96c5bf03f77844b044645d1da
Arvens <lklkk2002@naver.com>
2022-12-22 17:54:06 +0900

    algorithm basic_lv2 studying

--- test_file/algorithm/basic_l2.py
+++ test_file/algorithm/basic_l2.py
@@ -48,9 +48,71 @@
 
 # 19. 핸드폰 번호 가리기
 
-def solution(phone_number) :
-	result = list(phone_number)[-1:-4]
+# slicing
+# def solution(phone_number) :
+# 	return '*' * (len(phone_number) - 4) + phone_number[-4:len(phone_number):1]
+
+# 개선
+# def solution(phone_number) :
+# 	return '*' * (len(phone_number) - 4) + phone_number[-4:]
+
+# print(solution(input()))
+
+# 20. K번째 수
+
+# for
+# def solution(array, commands) :
+# 	result = []
+
+# 	for start, end, target in commands :
+# 		temp = array[start - 1: end]
+# 		temp.sort()
+# 		result.append(temp[target - 1])
+
+# 	return result
+
+# one_line + method(sorted) / lambda
+# def solution(array, commands) :
+# 	return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
+
+# arr, com = [1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+# print(solution(arr, com))
+
+# 21. 하샤드 수
+
+# for
+# def solution(number) :
+# 	result = 0
+
+# 	for i in str(number) :
+# 		result += int(i)
+
+# 	return True if number % result == 0 else False
+
+# one_line method
+# def solution(number) :
+# 	return number % sum([int(num) for num in str(number)]) == 0
+
+# print(solution(int(input())))
+
+# 22. 나누어 떨어지는 숫자 배열
+
+# 조건 나누어 떨어지는게 있는가?
+# 1. 모두 나누어 떨어진다 -> 정렬해서 넣는다.
+# 2. 한 개는 있다. -> 정렬해서 넣는다.
+# 3. 하나도 없다. -> -1를 반환한다.
+def solution(arr, divisor) :
+	result = []
+
+	for val in arr :
+		if val % divisor == 0 :
+			result.append(val)
+			result.sort()
+		# elif arr in (val % divisor != 0) :
+		# 	return result.append(-1)
 
 	return result
+	# return [val for val in arr if val % divisor == 0]
 
-print(solution(input()))
+arr = [10, 9, 7, 5]
+print(solution(arr, int(input())))
